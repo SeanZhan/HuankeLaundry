47447963ae8f507e0a83c652cd0934811ff27564
SeanZhan <zhanlixiang0729@163.com>
2015-11-29 15:10:10 +0800

    完成自定义按钮

---
+++ HuanKeLaundry/View/MyViewCustomButton.h
@@ -0,0 +1,13 @@
+//
+//  MyViewCustomButton.h
+//  HuanKeLaundry
+//
+//  Created by zhan on 15/11/29.
+//  Copyright © 2015年 湛礼翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewCustomButton : UIControl
+
+@end
---
+++ HuanKeLaundry/View/MyViewCustomButton.m
@@ -0,0 +1,21 @@
+//
+//  MyViewCustomButton.m
+//  HuanKeLaundry
+//
+//  Created by zhan on 15/11/29.
+//  Copyright © 2015年 湛礼翔. All rights reserved.
+//
+
+#import "MyViewCustomButton.h"
+
+@implementation MyViewCustomButton
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
