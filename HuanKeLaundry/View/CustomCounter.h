55f828b6a460aaa932ed6d59216cab3790f38199
SeanZhan <zhanlixiang0729@163.com>
2015-11-30 18:15:04 +0800

    完成模态窗口

---
+++ HuanKeLaundry/View/CustomCounter.h
@@ -0,0 +1,13 @@
+//
+//  CustomCounter.h
+//  HuanKeLaundry
+//
+//  Created by zhan on 15/11/30.
+//  Copyright © 2015年 湛礼翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCounter : UIView
+
+@end
---
+++ HuanKeLaundry/View/CustomCounter.m
@@ -0,0 +1,21 @@
+//
+//  CustomCounter.m
+//  HuanKeLaundry
+//
+//  Created by zhan on 15/11/30.
+//  Copyright © 2015年 湛礼翔. All rights reserved.
+//
+
+#import "CustomCounter.h"
+
+@implementation CustomCounter
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
