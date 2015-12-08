be66234e7b92c875677ea3df2caa8dcb37bfff39
SeanZhan <zhanlixiang0729@163.com>
2015-12-08 15:30:42 +0800

    给自定义的View添加属性观察

---
+++ HuanKeLaundry/View/BasketTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  BasketTableViewCell.h
+//  HuanKeLaundry
+//
+//  Created by zhan on 15/12/4.
+//  Copyright © 2015年 湛礼翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BasketTableViewCell : UITableViewCell
+
+@end
---
+++ HuanKeLaundry/View/BasketTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  BasketTableViewCell.m
+//  HuanKeLaundry
+//
+//  Created by zhan on 15/12/4.
+//  Copyright © 2015年 湛礼翔. All rights reserved.
+//
+
+#import "BasketTableViewCell.h"
+
+@implementation BasketTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
